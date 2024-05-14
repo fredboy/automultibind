9844eae92a582bae5c29e2d120681ebfec14844b
fredboy <fredboy@protonmail.com>
2024-05-15 04:31:28 +0700

    Use project dependencies

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -14,8 +14,8 @@ tasks {
 dependencies {
     api("com.google.dagger:dagger:${Versions.DAGGER}")
 
-    implementation("ru.fredboy:automultibind-annotations:1.0.0")
-    ksp("ru.fredboy:automultibind-ksp:1.0.0")
+    implementation(project(":automultibind-annotations"))
+    ksp(project(":automultibind-ksp"))
 
     ksp("com.google.dagger:dagger-compiler:${Versions.DAGGER}")
 }
